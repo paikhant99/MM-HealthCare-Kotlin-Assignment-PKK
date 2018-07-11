90f8456f53d6c1c1f070eb5de2333b8944a3d4b7
Pai Khant Ko <kopaikhant@gmail.com>
2018-07-11 13:54:19 +0630

    assignment setup

---
+++ app/src/main/java/com/padcmyanmar/mm_healthcare_kotlin_assignment_pkk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.padcmyanmar.mm_healthcare_kotlin_assignment_pkk
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
